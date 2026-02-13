8e69c75e7497ba6bbb7acbf0187fbf11457f4dda
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-02-13 09:09:00 +0000

    Corrected typo in decimal-number.type.ts

--- src/react-library/common/number/types/decimal-number.type.ts
+++ src/react-library/common/number/types/decimal-number.type.ts
@@ -1,4 +1,4 @@
 /**
- * Defines numbers for base 10 (ecimal) number system
+ * Defines numbers for base 10 (decimal) number system
  */
 export type NumberDecimal = (0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9);
