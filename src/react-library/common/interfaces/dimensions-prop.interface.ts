a82159833d438dbff4f05c7c9445c4ed2b488a44
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-02-16 11:57:59 +0000

    Deleted unused interface dimensions-prop.interface.ts

--- src/react-library/common/interfaces/dimensions-prop.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { IDimensions } from "./dimensions.interface";
-
-/**
- * For a component that can have a dimensions prop
- */
-export interface IDimensionsProp<THeight, TWidth> {
-	dimensions: IDimensions<THeight, TWidth>;
-}
