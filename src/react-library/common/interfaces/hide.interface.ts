f24296d29b8302f0825827637e0df245388577bd
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-02-19 16:42:39 +0000

    Added common package interfaces for hide.interface.ts and include-divider.interface.ts

---
+++ src/react-library/common/interfaces/hide.interface.ts
@@ -0,0 +1,6 @@
+/**
+ * For a component that has a hide state
+ */
+export interface IHide<THide> {
+	hide: THide;
+}
---
+++ src/react-library/common/interfaces/include-divider.interface.ts
@@ -0,0 +1,6 @@
+/**
+ * For a component that can have an include divider state
+ */
+export interface IIncludeDivider<TIncludeDivider> {
+	includeDivider: TIncludeDivider;
+}
