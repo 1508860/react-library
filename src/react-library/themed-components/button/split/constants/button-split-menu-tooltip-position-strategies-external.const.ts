7e2c6ed4b5e855973faf6408e9eff2a2739bb270
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-02-25 12:05:39 +0000

    Missing file from previous commit for renamed button-split-menu-tooltip-position-strategies-external.const.ts after tooltips refactor

---
+++ src/react-library/themed-components/button/split/constants/button-split-menu-tooltip-position-strategies-external.const.ts
@@ -0,0 +1,12 @@
+import { PositionStrategyExternal } from "@react-library/common";
+import type { TooltipPositionStrategiesExternal } from "@react-library/components";
+
+/**
+ * Position strategy external for split button menu tooltip
+ */
+export const BUTTON_SPLIT_MENU_TOOLTIP_POSITION_STRATEGIES_EXTERNAL: TooltipPositionStrategiesExternal = [
+	{ positionType: PositionStrategyExternal.BottomEnd, marginTop: 4 },
+	{ positionType: PositionStrategyExternal.BottomStart, marginTop: 4 },
+	{ positionType: PositionStrategyExternal.TopEnd, marginBottom: 4 },
+	{ positionType: PositionStrategyExternal.TopStart, marginBottom: 4 }
+];
