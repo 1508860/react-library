bc002b8ef3f336e8ed16d35b8a350faf4056d9fb
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-03-03 14:01:26 +0000

    Fixed incorrect use of deleted prop from demo-column-container-style.function.ts and corrected name of style function

--- src/react-library-demo/shared/components/demo-column-container/demo-column-container-style.function.ts
+++ src/react-library-demo/shared/components/demo-column-container/demo-column-container-style.function.ts
@@ -1,11 +1,11 @@
 import type { CSSProperties } from "react";
+
 import type { DemoColumnContainerProps } from "./demo-column-container-props.type";
 
-export function demoColumnStyle(props: DemoColumnContainerProps): CSSProperties {
+export function demoColumnContainerStyle(props: DemoColumnContainerProps): CSSProperties {
 	return {
 		display: "flex",
 		flexDirection: "row",
-		height: props.height,
 		overflow: "hidden",
 		width: props.width
 	};
--- src/react-library-demo/shared/components/demo-column-container/demo-column-container.tsx
+++ src/react-library-demo/shared/components/demo-column-container/demo-column-container.tsx
@@ -1,9 +1,9 @@
-import { demoColumnStyle } from "./demo-column-container-style.function";
+import { demoColumnContainerStyle } from "./demo-column-container-style.function";
 import type { DemoColumnContainerProps } from "./demo-column-container-props.type";
 
 export function DemoColumnContainer(props: DemoColumnContainerProps) {
 	return (
-		<div style={demoColumnStyle(props)}>
+		<div style={demoColumnContainerStyle(props)}>
 			{props.children}
 		</div>
 	);
