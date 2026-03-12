c1bc438542b683454b0a986c1d69003b95c1c53b
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-03-12 12:16:22 +0000

    Updated common namespace to https

--- src/react-library/common/w3c/svg-namespace.const.ts
+++ src/react-library/common/w3c/svg-namespace.const.ts
@@ -1,4 +1,4 @@
 /**
  * Contant for the W3C namespace for SVGs
  */
-export const SVG_NAMESPACE: string = "http://www.w3.org/2000/svg";
+export const SVG_NAMESPACE: string = "https://www.w3.org/2000/svg";
