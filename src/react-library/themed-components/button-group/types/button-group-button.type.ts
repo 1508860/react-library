dda01ef48bf745969d346f381948b41fe813eb2c
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-03-18 15:26:37 +0000

    Tidied up the button group themed component button-group-button.type.ts to use the newly created content type for various buttons

--- src/react-library/themed-components/button-group/types/button-group-button.type.ts
+++ src/react-library/themed-components/button-group/types/button-group-button.type.ts
@@ -1,13 +1,11 @@
 import type { Attributes } from "react";
 
 import type { IAction, IIsDisabled, IIsSelected, IOnClick } from "@react-library/common";
-import type { MaterialIconName, MaterialIconStyle } from "@react-library/material-icons";
 
 import type {
-	ButtonContentIcon,
-	ButtonContentIconLabel,
-	ButtonContentIconWithWidth,
-	ButtonContentLabel,
+	ButtonClickContent,
+	ButtonSelectionContent,
+	ButtonSplitContent,
 	ButtonSplitMenuElement
 } from "../../button";
 import type { ButtonGroupAction } from "../enums/button-group-action.type";
@@ -22,31 +20,25 @@ type ButtonGroupButtonBase = (
 );
 
 /**
- * Describes click configuration for a button group's button
+ * Describes base type configuration for a button group's button action
  */
-export type ButtonGroupButtonClick = (
-	(
-		ButtonContentIconWithWidth<MaterialIconName, MaterialIconStyle> |
-		ButtonContentIconLabel<MaterialIconName, MaterialIconStyle> |
-		ButtonContentLabel
-	) &
-	ButtonGroupButtonBase
+type ButtonGroupButtonActionBase<TButtonGroupAction extends ButtonGroupAction> = (
+	IAction<TButtonGroupAction>
 );
 
 /**
- * Describes click configuration for a button group's button where multiple button actions can be in a group
+ * Describes click configuration for a button group's button
  */
-export type ButtonGroupButtonClickWithAction = (IAction<typeof ButtonGroupAction.Click> & ButtonGroupButtonClick);
+export type ButtonGroupButtonClick = (
+	ButtonClickContent &
+	ButtonGroupButtonBase
+);
 
 /**
  * Describes selection configuration for a button group's button
  */
 export type ButtonGroupButtonSelection = (
-	(
-		ButtonContentIconWithWidth<MaterialIconName, MaterialIconStyle> |
-		ButtonContentIconLabel<MaterialIconName, MaterialIconStyle> |
-		ButtonContentLabel
-	) &
+	ButtonSelectionContent &
 	ButtonGroupButtonBase &
 	IIsSelected
 );
@@ -55,16 +47,23 @@ export type ButtonGroupButtonSelection = (
  * Describes split configuration for a button group's button
  */
 export type ButtonGroupButtonSplit = (
-	(
-		ButtonContentIcon<MaterialIconName, MaterialIconStyle> |
-		ButtonContentIconLabel<MaterialIconName, MaterialIconStyle> |
-		ButtonContentLabel
-	) &
+	ButtonSplitContent &
 	ButtonGroupButtonBase &
 	ButtonSplitMenuElement
 );
 
 /**
+ * Describes click configuration for a button group's button where multiple button actions can be in a group
+ */
+export type ButtonGroupButtonClickWithAction = (
+	ButtonGroupButtonActionBase<typeof ButtonGroupAction.Click> &
+	ButtonGroupButtonClick
+);
+
+/**
  * Describes split configuration for a button group's button where multiple button actions can be in a group
  */
-export type ButtonGroupButtonSplitWithAction = (IAction<typeof ButtonGroupAction.Split> & ButtonGroupButtonSplit);
+export type ButtonGroupButtonSplitWithAction = (
+	ButtonGroupButtonActionBase<typeof ButtonGroupAction.Split> &
+	ButtonGroupButtonSplit
+);
