a77bc69bc210441f5733e53fe69a3cec709c46be
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-03-21 15:31:45 +0000

    Updated name for default icon svg paths map of default

--- src/react-library/material-icons/enums/material-icon-style-map.type.ts
+++ src/react-library/material-icons/enums/material-icon-style-map.type.ts
@@ -2,13 +2,13 @@ import type { ArrayMinLength1 } from "@react-library/common";
 
 import type { MaterialIconName } from "./material-icon-name.type";
 import { MaterialIconStyle } from "./material-icon-style.type";
+import { MaterialIconSvgPathDefault } from "./material-icon-svg-path-default.type";
 import { MaterialIconSvgPathThick } from "./material-icon-svg-path-thick.type";
-import { MaterialIconSvgPath } from "./material-icon-svg-path.type";
 
 /**
  * Material icon style map to retiurn relevant set of icon SVG paths for {@link MaterialIconName} icons
  */
 export const MaterialIconStyleMap = {
-	[MaterialIconStyle.Default]: MaterialIconSvgPath,
+	[MaterialIconStyle.Default]: MaterialIconSvgPathDefault,
 	[MaterialIconStyle.Thick]: MaterialIconSvgPathThick
 } as const satisfies Record<MaterialIconStyle, Record<MaterialIconName, ArrayMinLength1<string>>>;
--- src/react-library/material-icons/enums/material-icon-svg-path-default.type.ts
+++ src/react-library/material-icons/enums/material-icon-svg-path-default.type.ts
@@ -3,14 +3,14 @@ import type { ArrayMinLength1 } from "@react-library/common";
 import { MaterialIconName } from "./material-icon-name.type";
 
 /**
- * Type for {@link MaterialIconSvgPath}
+ * Type for {@link MaterialIconSvgPathDefault}
  */
-export type MaterialIconSvgPath = (typeof MaterialIconSvgPath)[keyof typeof MaterialIconSvgPath];
+export type MaterialIconSvgPathDefault = (typeof MaterialIconSvgPathDefault)[keyof typeof MaterialIconSvgPathDefault];
 
 /**
  * Type for material icon SVG data to draw the icon from as an outline
  */
-export const MaterialIconSvgPath = {
+export const MaterialIconSvgPathDefault = {
 	[MaterialIconName.Add]: ["M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"],
 	[MaterialIconName.ArrowBack]: ["m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"],
 	[MaterialIconName.CheckCircle]: ["m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"],
