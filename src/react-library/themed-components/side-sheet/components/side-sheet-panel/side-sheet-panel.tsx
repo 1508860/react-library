1856c44e05e55de4ef0c62f55cb286de7f4a50f7
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-03-26 16:28:27 +0000

    Updated side sheet themed component to use the transition size component to handle the expansion of the side sheet

--- src/react-library/themed-components/side-sheet/components/side-sheet-panel/side-sheet-panel.tsx
+++ src/react-library/themed-components/side-sheet/components/side-sheet-panel/side-sheet-panel.tsx
@@ -1,6 +1,14 @@
 import { Orientation, useColourSchemeContext } from "@react-library/common";
+import {
+	TransitionSize,
+	TransitionSizeDirection,
+	TransitionTiming
+} from "@react-library/components";
 
 import { Divider } from "../../../divider";
+
+import { SIDE_SHEET_PANEL_TRANSITION_DURATION_MS } from "../../constants/side-sheet-panel-transition-duration-ms.const";
+
 import { SideSheetPanelBody } from "../side-sheet-panel-body";
 import { SideSheetPanelFooter } from "../side-sheet-panel-footer";
 import { SideSheetPanelHeader } from "../side-sheet-panel-header";
@@ -15,34 +23,42 @@ export function SideSheetPanel(props: SideSheetPanelProps) {
 	const colourScheme = useColourSchemeContext();
 
 	return (
-		<div style={sideSheetPanelStyle(props.show, props.width, colourScheme)}>
-			<SideSheetPanelHeader
-				key="header"
-				onBack={props.onBack}
-				onClose={props.onClose}
-				show={props.showContent}
-				title={props.title}
-				width={props.width}
-			/>
-			<SideSheetPanelBody
-				body={props.body}
-				includeBodyPaddingBottom={props.includeBodyPaddingBottom && props.showFooterDivider && !!props.footer}
-				includeBodyPaddingHorizontal={props.includeBodyPaddingHorizontal}
-				key="body"
-				show={props.showContent}
-				width={props.width}
-			/>
-			<Divider
-				hide={(!props.showFooterDivider || !props.footer)}
-				key="footer-divider"
-				orientation={Orientation.Horizontal}
-			/>
-			<SideSheetPanelFooter
-				key="footer"
-				footer={props.footer}
-				show={props.showContent}
-				width={props.width}
-			/>
-		</div>
+		<TransitionSize
+			direction={TransitionSizeDirection.Horizontal}
+			durationMs={SIDE_SHEET_PANEL_TRANSITION_DURATION_MS}
+			timing={TransitionTiming.EaseInOut}
+			height={"100%"}
+			width={props.show ? props.width : 0}
+		>
+			<div style={sideSheetPanelStyle(colourScheme)}>
+				<SideSheetPanelHeader
+					key="header"
+					onBack={props.onBack}
+					onClose={props.onClose}
+					show={props.showContent}
+					title={props.title}
+					width={props.width}
+				/>
+				<SideSheetPanelBody
+					body={props.body}
+					includeBodyPaddingBottom={props.includeBodyPaddingBottom && props.showFooterDivider && !!props.footer}
+					includeBodyPaddingHorizontal={props.includeBodyPaddingHorizontal}
+					key="body"
+					show={props.showContent}
+					width={props.width}
+				/>
+				<Divider
+					hide={(!props.showFooterDivider || !props.footer)}
+					key="footer-divider"
+					orientation={Orientation.Horizontal}
+				/>
+				<SideSheetPanelFooter
+					key="footer"
+					footer={props.footer}
+					show={props.showContent}
+					width={props.width}
+				/>
+			</div>
+		</TransitionSize>
 	);
 }
--- src/react-library/themed-components/side-sheet/components/side-sheet-panel/styles/side-sheet-panel-style.function.ts
+++ src/react-library/themed-components/side-sheet/components/side-sheet-panel/styles/side-sheet-panel-style.function.ts
@@ -1,28 +1,21 @@
 import type { CSSProperties } from "react";
 
-import type { ColourScheme, Size } from "@react-library/common";
-
-import { SIDE_SHEET_PANEL_TRANSITION_DURATION_MS } from "../../../constants/side-sheet-panel-transition-duration-ms.const";
+import type { ColourScheme } from "@react-library/common";
 
 /**
  * Resolve side sheet panel style based on parameters
- * @param show
- * @param width
  * @param colourScheme
  */
-export function sideSheetPanelStyle(show: boolean, width: Size, colourScheme: ColourScheme): CSSProperties {
+export function sideSheetPanelStyle(colourScheme: ColourScheme): CSSProperties {
 	return {
 		alignItems: "stretch",
 		backgroundColor: colourScheme.surface.default.colour.toColourString(),
 		display: "flex",
 		flexDirection: "column",
 		flexShrink: 0,
-		maxWidth: 400,
+		height: "100%",
 		overflow: "hidden",
 		position: "relative",
-		transitionDuration: `${SIDE_SHEET_PANEL_TRANSITION_DURATION_MS}ms`,
-		transitionProperty: "width",
-		transitionTimingFunction: "ease-in-out",
-		width: show ? width : 0
+		width: "100%"
 	};
 }
