111d27fad60aa48b16f10ff1f51911997116c0e2
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-03-26 09:11:51 +0000

    Improved types for common border radius style

--- src/react-library/common/style/border-radius/border-radius-corner.type.ts
+++ src/react-library/common/style/border-radius/border-radius-corner.type.ts
@@ -1,11 +1,11 @@
+import type { Size, SizeWithUnits } from "../../units";
 import type { StyleGlobal } from "../global";
-import type { StyleBorderRadiusSize } from "./style-border-radius-size.type";
 
 /**
  * Type to describe thestyle format of a border radius for a single corner of an element
  */
 export type StyleBorderRadiusCorner = (
 	StyleGlobal |
-	`${StyleBorderRadiusSize}` |
-	`${StyleBorderRadiusSize} ${StyleBorderRadiusSize}`
+	`${Size}` |
+	`${SizeWithUnits} ${SizeWithUnits}`
 );
