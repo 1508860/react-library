58185540289c7221a1010fadcd2369ea337991d5
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-04-09 10:40:40 +0100

    Corrected unit types in some demo constants

--- src/react-library-demo/routes/themed-components/modals/constants/modals-demo-size.const.ts
+++ src/react-library-demo/routes/themed-components/modals/constants/modals-demo-size.const.ts
@@ -1,1 +1,9 @@
-export const MODALS_DEMO_SIZE_PX: number = 300;
+import type { SizePx } from "@react-library/common";
+
+export const MODALS_DEMO_SIZE_PX: SizePx = 100;
+
+export const MODALS_DEMO_BASIC_CHILDREN_SIZE_PX: SizePx = 500;
+
+export const MODALS_DEMO_BASIC_CHILDREN_OVERFLOW_SIZE_PX: SizePx = MODALS_DEMO_BASIC_CHILDREN_SIZE_PX * 2;
+
+export const MODALS_DEMO_BASIC_CHILDREN_TEXT_MAX_WIDTH_PX: SizePx = 200;
--- src/react-library-demo/routes/themed-components/tooltips/constants/tooltips-demo-size.const.ts
+++ src/react-library-demo/routes/themed-components/tooltips/constants/tooltips-demo-size.const.ts
@@ -1,3 +1,5 @@
-export const TOOLTIPS_DEMO_SIZE_PX: number = 300;
+import type { SizePx } from "@react-library/common";
 
-export const TOOLTIPS_DEMO_SUBJECT_SIZE_PX: number = TOOLTIPS_DEMO_SIZE_PX / 2;
+export const TOOLTIPS_DEMO_SIZE_PX: SizePx = 300;
+
+export const TOOLTIPS_DEMO_SUBJECT_SIZE_PX: SizePx = TOOLTIPS_DEMO_SIZE_PX / 2;
