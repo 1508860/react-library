956fc6d1837bd25274c2c25979c8eba2a58a25bb
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-04-09 11:44:11 +0100

    Updated basic modal themed component demo to show use cases for this modal variant

--- src/react-library-demo/routes/themed-components/modals/components/modals-basic-demo.tsx
+++ src/react-library-demo/routes/themed-components/modals/components/modals-basic-demo.tsx
@@ -1,6 +1,20 @@
-import { Orientation } from "@react-library/common";
+import { Fragment, useCallback, useEffect, useState } from "react";
 
 import {
+	Orientation,
+	type Callback,
+	type IActionConfirm,
+	type IChild,
+	type IIconName,
+	type IIsFullWidth,
+	type ITextMaxWidth,
+	type ITitle
+} from "@react-library/common";
+import { ModalBasic, ModalBasicActionDismissText } from "@react-library/themed-components";
+import { MaterialIconName } from "@react-library/material-icons";
+
+import {
+	DEMO_LOREM_IPSUM,
 	DemoContent,
 	DemoContentAlign,
 	DemoContentChildren,
@@ -8,32 +22,164 @@ import {
 	DemoContentColourScheme,
 	DemoContentJustify,
 	DemoContentOverflow,
-	DemoSection
+	DemoItem,
+	DemoSection,
+	type DemoItemConfigCollection
 } from "@react-library-demo/shared";
 
-import { MODALS_DEMO_SIZE_PX } from "../constants";
+import {
+	MODALS_DEMO_BASIC_CHILDREN_OVERFLOW_SIZE_PX,
+	MODALS_DEMO_BASIC_CHILDREN_SIZE_PX,
+	MODALS_DEMO_BASIC_CHILDREN_TEXT_MAX_WIDTH_PX,
+	MODALS_DEMO_SIZE_PX
+} from "../constants";
 
 export function ReactLibraryThemedComponentsModalsBasicDemo() {
+
 	return (
 		<DemoSection title="Modals - Basic">
-			<DemoContent
-				align={DemoContentAlign.Center}
-				childrenType={DemoContentChildren.Items}
-				colourScheme={DemoContentColourScheme.Primary}
-				height={MODALS_DEMO_SIZE_PX}
-				items={[
-					{
-						type: DemoContentChildrenItem.Button,
-						action: () => { },
-						id: 1,
-						text: "Open basic modal"
-					}
-				]}
-				justify={DemoContentJustify.Center}
-				orientation={Orientation.Vertical}
-				overflow={DemoContentOverflow.Auto}
-				width={MODALS_DEMO_SIZE_PX}
+			<ModalsBasicDemoItem />
+			<ModalsBasicDemoItem
+				actionConfirm={true}
+			/>
+			<ModalsBasicDemoItem
+				title={true}
+			/>
+			<ModalsBasicDemoItem
+				iconName={true}
+			/>
+			<ModalsBasicDemoItem
+				iconName={true}
+				title={true}
+			/>
+			<ModalsBasicDemoItem
+				textMaxWidth={true}
+				title={true}
+			/>
+			<ModalsBasicDemoItem
+				iconName={true}
+				textMaxWidth={true}
+				title={true}
+			/>
+			<ModalsBasicDemoItem
+				actionConfirm={true}
+				children={true}
+				title={true}
+			/>
+			<ModalsBasicDemoItem
+				actionConfirm={true}
+				children={true}
+				isFullWidth={true}
+				title={true}
+			/>
+			<ModalsBasicDemoItem
+				actionConfirm={true}
+				children={true}
+				iconName={true}
+				isFullWidth={true}
+				title={true}
 			/>
 		</DemoSection>
+	);
+}
+
+function ModalsBasicDemoItem(props: (
+	Partial<IActionConfirm<boolean>> &
+	Partial<IChild<boolean>> &
+	Partial<IIconName<boolean>> &
+	Partial<IIsFullWidth<boolean>> &
+	Partial<ITextMaxWidth<boolean>> &
+	Partial<ITitle<boolean>>
+)) {
+
+	const resolveDemoItemConfig = useCallback<Callback<DemoItemConfigCollection>>(
+		() => {
+			const result: DemoItemConfigCollection = [];
+			if (props.actionConfirm) result.push({ key: "actionConfirm", value: props.actionConfirm })
+			if (props.children) result.push({ key: "children", value: props.children })
+			if (props.iconName) result.push({ key: "iconName", value: props.iconName })
+			if (props.isFullWidth) result.push({ key: "isFullWidth", value: props.isFullWidth })
+			if (props.textMaxWidth) result.push({ key: "textMaxWidth", value: props.textMaxWidth })
+			if (props.title) result.push({ key: "title", value: props.title })
+			return result;
+		},
+		[props]
+	);
+	const [demoItemConfig, setDemoItemConfig] = useState<DemoItemConfigCollection>(() => resolveDemoItemConfig());
+	useEffect(() => setDemoItemConfig(resolveDemoItemConfig()), [resolveDemoItemConfig]);
+
+	const [showModal, setShowModal] = useState<boolean>(() => false);
+	const handleHideModal = useCallback<Callback<void>>(() => setShowModal(false), []);
+	const handleShowModal = useCallback<Callback<void>>(() => setShowModal(true), []);
+
+	return (
+		<>
+			<DemoItem
+				config={demoItemConfig}
+				height={MODALS_DEMO_SIZE_PX}
+				key="demo-content"
+				label={demoItemConfig.length === 0 ? "Basic" : undefined}
+				width={MODALS_DEMO_SIZE_PX}
+			>
+				<DemoContent
+					align={DemoContentAlign.Center}
+					childrenType={DemoContentChildren.Items}
+					colourScheme={DemoContentColourScheme.Primary}
+					height="100%"
+					items={[
+						{
+							type: DemoContentChildrenItem.Button,
+							action: handleShowModal,
+							id: 1,
+							text: "Open"
+						}
+					]}
+					justify={DemoContentJustify.Center}
+					orientation={Orientation.Vertical}
+					overflow={DemoContentOverflow.Auto}
+					width="100%"
+				/>
+			</DemoItem>
+			{
+				showModal ?
+					<ModalBasic
+						actionConfirm={props.actionConfirm ? { action: handleHideModal, text: "Confirm" } : undefined}
+						actionDismiss={{ action: handleHideModal, text: ModalBasicActionDismissText.Close }}
+						iconName={props.iconName ? MaterialIconName.Delete : undefined}
+						isFullWidth={props.isFullWidth}
+						key="modal-basic"
+						text={DEMO_LOREM_IPSUM}
+						textMaxWidth={props.textMaxWidth ? MODALS_DEMO_BASIC_CHILDREN_TEXT_MAX_WIDTH_PX : undefined}
+						title={props.title ? "Title" : undefined}
+					>
+						{
+							props.children ?
+								<DemoContent
+									align={DemoContentAlign.Center}
+									childrenType={DemoContentChildren.Any}
+									colourScheme={DemoContentColourScheme.Secondary}
+									height={MODALS_DEMO_BASIC_CHILDREN_SIZE_PX}
+									justify={DemoContentJustify.Center}
+									orientation={Orientation.Vertical}
+									overflow={DemoContentOverflow.Auto}
+									width="100%"
+								>
+									<DemoContent
+										align={DemoContentAlign.Center}
+										childrenType={DemoContentChildren.Text}
+										colourScheme={DemoContentColourScheme.Secondary}
+										height={MODALS_DEMO_BASIC_CHILDREN_OVERFLOW_SIZE_PX}
+										justify={DemoContentJustify.Center}
+										orientation={Orientation.Vertical}
+										overflow={DemoContentOverflow.Auto}
+										text="Custom demo content"
+										width="100%"
+									/>
+								</DemoContent> : undefined
+						}
+					</ModalBasic> :
+					<Fragment key="no-modal-basic" />
+			}
+		</>
 	);
 }
