b4e8516b7e2ed0283efd6865d9610e7255e2f073
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-04-16 16:16:06 +0100

    Corrected incorrect comments for the colour scheme and font contexts in the common package

--- src/react-library/common/colours/scheme/constants/colour-scheme-context.const.ts
+++ src/react-library/common/colours/scheme/constants/colour-scheme-context.const.ts
@@ -3,6 +3,6 @@ import { type Context, createContext } from "react";
 import type { ColourScheme } from "../types/colour-scheme.type";
 
 /**
- * Context to provide {@link CurrencyCode}
+ * Context to provide {@link ColourScheme}
  */
 export const COLOUR_SCHEME_CONTEXT: Context<ColourScheme | null> = createContext<ColourScheme | null>(null);
--- src/react-library/common/fonts/font-context.const.ts
+++ src/react-library/common/fonts/font-context.const.ts
@@ -3,6 +3,6 @@ import { type Context, createContext } from "react";
 import type { Font } from "./font.type";
 
 /**
- * Context to provide {@link CurrencyCode}
+ * Context to provide {@link Font}
  */
 export const FONT_CONTEXT: Context<Font | null> = createContext<Font | null>(null);
