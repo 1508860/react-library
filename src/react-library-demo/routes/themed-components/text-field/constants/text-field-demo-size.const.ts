942eb17c23c2025e24c152d1e418809eaa21f5fa
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-05-09 15:54:49 +0100

    Removed unused files

--- src/react-library-demo/routes/themed-components/text-field/constants/text-field-demo-size.const.ts
+++
@@ -1,3 +0,0 @@
-import type { SizePx } from "@react-library/common";
-
-export const TEXT_FIELD_DEMO_SIZE_PADDING: SizePx = 50;
--- src/react-library/themed-components/checkbox/shared/constants/checkbox-transition.const.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Duration of any transitions for the checkbox
- */
-export const CHECKBOX_TRANSITION_DURATION_MS: number = 500;
