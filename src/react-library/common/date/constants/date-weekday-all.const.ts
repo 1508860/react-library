e1701b64d9459ab704e1a93ebc70b58dcdee42b2
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-05-27 16:02:57 +0100

    Updated common package to use and export the date weekdays type to describe the length of an array of days of the week

--- src/react-library/common/date/constants/date-weekday-all.const.ts
+++ src/react-library/common/date/constants/date-weekday-all.const.ts
@@ -1,16 +1,15 @@
-import type { ArrayFixedLength7 } from "../../types";
-
 import { DateWeekday } from "../enums/date-weekday.type";
+import type { DateWeekdays } from "../types/date-weekdays.type";
 
 /**
  * Full array of {@link DateWeekday}
  */
-export const DATE_WEEKDAY_ALL: ArrayFixedLength7<DateWeekday> = [
+export const DATE_WEEKDAY_ALL: DateWeekdays = [
 	DateWeekday.Sunday,
 	DateWeekday.Monday,
 	DateWeekday.Tuesday,
 	DateWeekday.Wednesday,
 	DateWeekday.Thursday,
 	DateWeekday.Friday,
-	DateWeekday.Saturday,
+	DateWeekday.Saturday
 ];
--- src/react-library/common/date/index.ts
+++ src/react-library/common/date/index.ts
@@ -22,3 +22,4 @@ export * from "./hooks/display/use-weekday-display-state.hook";
 export * from "./hooks/display/use-weekday-map-display-state.hook";
 export * from "./types/date-time-display-state.type";
 export * from "./types/date-time-value.type";
+export * from "./types/date-weekdays.type";
