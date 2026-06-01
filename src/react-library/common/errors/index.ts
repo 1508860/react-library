29d28558d01bb7d65f6f84627a0c8d583e0c97fd
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-06-01 15:49:36 +0100

    Removed unused virtual scroll error from the common package

--- src/react-library/common/errors/index.ts
+++ src/react-library/common/errors/index.ts
@@ -1,6 +1,5 @@
 export * from "./month-invalid.error";
 export * from "./null-context.error";
 export * from "./scale-degrees.error";
-export * from "./virtual-scroll.error";
 export * from "./weekday-invalid.error";
 export * from "./wrap-array-index.error";
