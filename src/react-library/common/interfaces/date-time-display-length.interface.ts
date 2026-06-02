4cb40d3738448c4b1bbdd709900c097ad2c52f3b
Gareth Evans <gareth.evans@Redrow.co.uk>
2026-06-02 16:05:37 +0100

    Added common package interfaces for the text field date themed component

---
+++ src/react-library/common/interfaces/date-time-display-length.interface.ts
@@ -0,0 +1,6 @@
+/**
+ * For a component that can have a date time display length
+ */
+export interface IDateTimeDisplayLength<TDateTimeDisplayLength> {
+	dateTimeDisplayLength: TDateTimeDisplayLength;
+}
---
+++ src/react-library/common/interfaces/modal-text.interface.ts
@@ -0,0 +1,6 @@
+/**
+ * Common interface to describe a component with a modal text state
+ */
+export interface IModalText<TModalText> {
+	modalText: TModalText;
+}
